3e1a562e4e53d17043be978ec4f8f7c475b9b815
dandelion-huang <dandelion820308@gmail.com>
2024-04-25 15:02:18 +0800

    refactor(ui): update the import paths of fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,19 +3,19 @@ import localFont from 'next/font/local';
 export const sourceSansPro = localFont({
   src: [
     {
-      path: './fonts/source-sans-pro/SourceSansPro-ExtraLight.otf.woff2',
+      path: './../../public/fonts/source-sans-pro/SourceSansPro-ExtraLight.otf.woff2',
       weight: '200',
     },
     {
-      path: './fonts/source-sans-pro/SourceSansPro-Light.otf.woff2',
+      path: './../../public/fonts/source-sans-pro/SourceSansPro-Light.otf.woff2',
       weight: '300',
     },
     {
-      path: './fonts/source-sans-pro/SourceSansPro-Regular.otf.woff2',
+      path: './../../public/fonts/source-sans-pro/SourceSansPro-Regular.otf.woff2',
       weight: '400',
     },
     {
-      path: './fonts/source-sans-pro/SourceSansPro-Semibold.otf.woff2',
+      path: './../../public/fonts/source-sans-pro/SourceSansPro-Semibold.otf.woff2',
       weight: '600',
     },
   ],
